4cedddfa1bfbb2585407f0e4f1da18a6d15eaef6
BenQ <nghiavan0610@gmail.com>
2023-07-11 18:27:27 +0700

    Complete API

---
+++ api/src/test/java/com/benisme/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benisme.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
